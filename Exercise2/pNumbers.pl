d29bebc31d8097d4e5f38eef8c798c628ecb7785
brianmontero18 <brianmontero18@gmail.com>
2015-10-26 16:45:18 -0300

    Second Release!

---
+++ Exercise2/pNumbers.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+my @result;
+my $number;
+my $counter;
+
+print "Introduce a number between 2 and 1000000\n",
+$number= <>;
+for (my $varA =  $number; $varA >0; $varA--){
+	$counter=0;
+	for (my $varB = $varA; $varB >0; $varB--){	
+		if(($varA % $varB)==0){
+			$counter++;
+			print "$counter";
+		}	
+	}
+	if(counter == 2){
+		push @result, $varA;
+	}
+}
+print "The numbers are: @result\n",
+	
+__END__
