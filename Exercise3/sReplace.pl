b1bd6d03cd6340f831005432b3cbcd3e7cd85e3d
brianmontero18 <brianmontero18@gmail.com>
2015-10-28 10:35:11 -0300

    finished!

--- Exercise3/sReplace.pl
+++ Exercise3/sReplace.pl
@@ -5,7 +5,6 @@ use warnings;
 use Data::Dumper qw(Dumper);
 use 5.010;
 
-my $matching;
 my @aux;
 my $text;
 my @text;
@@ -18,32 +17,23 @@ print "Please, enter 1 line of text:";
 $text = <STDIN>;
 chomp($text);
 @text = split(/\s+/, $text);
-say Dumper\@text;
 print "Please, enter space separated strings to match and replace (one pair per line).\n";
 print "Empty line will interrupt input and start execution:\n";
 while(<>){
-	#$aux= <>;
 	if($_ eq "\n"){
 		last;
 	}
 	$replace= $_;
 	chomp($replace);
 	@aux = split(/\s+/, $replace);
-	#print "$aux";
-	#INVESTIGAAAAAAAAAAAAAAAARRR push @{$replace}, @aux;
+	push @replace, @aux;
 }
-say Dumper\@replace;
 %hash = @replace;
-say Dumper\%hash;
 @changes = keys %hash;
 foreach my $i (0 .. $#text){
 	for my $change (@changes){
 		if($text[$i] eq $change){
-			#print "$text[$i]";
-			#print "$replace[$j+1]";
-			#my $temp = $replace[$j+1];
 			$text[$i] = $hash{$change};
-			print "$text[$i]";
 		}
 	}
 }
