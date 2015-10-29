0ad91331ce8cc09b939a34e35d0dbac79cf5e026
brianmontero18 <brianmontero18@gmail.com>
2015-10-29 11:19:36 -0300

    actualization

--- Exercise6/fRenderer.pl
+++ Exercise6/fRenderer.pl
@@ -1,69 +1,50 @@
-e strict;
-	use warnings FATAL => 'all';
-	use Paths::Graph;
-	use Data::Dumper;
-	
-	my %tree;
-	my $start;
-	my $goal;
-	
-	sub has_key{
-	my %hash = %{shift()};
-	my $key = shift;
-	if (exists $hash{$key}) {
-	return 1;
-	}
-	foreach my $x (keys %hash) {
-	if (ref $hash{$x} eq ref {} ) {
-	my %tpmh = %{$hash{$x}};
-	return has_key(\%tpmh, $key);
-	}
-	}
-	return 0;
-	}
-	
-	sub set_value_for_key{
-	my %hash = %{shift()};
-	my $key = shift;
-	my $value = shift;
-	my @path = (keys %hash)[0];
-	if (exists $hash{$key}) {
-	$hash{$key}{$value}={};
-	}
-	foreach my $x (keys %hash) {
-	if (ref $hash{$x} eq ref {} ) {
-	my %tpmh = %{$hash{$x}};
-	%hash = (%hash, set_value_for_key(\%tpmh, $key, $value));
-	}
-	}
-	return %hash;
-	}
-	
-	print STDOUT "Enter the start and final node:\n";
-	my $input = <STDIN>;
-	chomp($input);
-	($start, $goal) = split(/\s/,$input);
-	print STDOUT "Enter the nodes: one coneion per line, space separated \n";
-	while(my $t = <STDIN>){
-	if($t eq "\n" || $t eq ""){
-	last;
-	}
-	my $tmpr;
-	my $tmpl;
-	($tmpr, $tmpl) = split(/\s/,$t);
-	if (has_key(\%tree, $tmpr)) {
-	%tree = set_value_for_key(\%tree, $tmpr, $tmpl);
-	}else{
-	$tree{$tmpr}={};
-	$tree{$tmpr}{$tmpl}={};
-	}
-	
-	}
-	
-	my $obj = Paths::Graph->new(-origin=>$start,-destiny=>$goal,-graph=>\%tree);
-	
-	my @way = $obj->shortest_path();
-	my $path = scalar @{$way[0]};
-	$path = $path -1;
-	
-	print STDOUT "$path\n";
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+package Polygon;
+use Data::Dumper;
+
+sub new{
+	my $class = shift;
+	my %args = @_;
+	my $self = bless{}, $class;
+	$self->
+	$self->
+	$self->
+
+	return $self;
+}
+
+sub add_vertex{
+	my $self = shift;
+	my $vertex = shift;
+	push $self->{vertices}, $point;
+}
+
+
+1;
+
+package Circle;our @ISA = qw(Figure);
+
+sub new{
+	my $class = shift;
+	my %args = @_;
+	my $self = $class->SUPER::new(polygon_type=>"circle", color=>"yellow");
+	$self->{radio}=$args{radio}||0;
+	$self->{center}=$args{center}||"0,0";
+	return $self;
+}
+1;
+
+package Circle;our @ISA = qw(Figure);
+
+sub new{
+	my $class = shift;
+	my %args = @_;
+	my $self = $class->SUPER::new(polygon_type=>"circle", color=>"yellow");
+	$self->{radio}=$args{radio}||0;
+	$self->{center}=$args{center}||"0,0";
+	return $self;
+}
+1;
